3752b69768cdaf07a8d572b7815628ec01a5e0b3
treyy3@bitbucket.org <treyy333@gmail.com>
2015-08-27 17:35:11 -0700

    count.js done

--- app/count.js
+++ app/count.js
@@ -1,7 +1,20 @@
 exports = (typeof window === 'undefined') ? global : window;
 
-exports.countAnswers =  {
-  count : function (start, end) {
-
-  }
+exports.countAnswers = {
+    /** gotta read the test code to know it wants console.log */
+    count: function (start, end) {
+        var isCanceled = false;
+        function doit() {
+            if (!isCanceled && start <= end) {
+                console.log(start++); 
+                setTimeout(doit, 100);
+            }
+        }
+        doit();
+        return {
+            cancel: function cancel() {
+                isCanceled = true;
+            }
+        }
+    }
 };
